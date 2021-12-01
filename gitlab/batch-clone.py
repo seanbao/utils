8cbe07ca92a99f659743469cf58ae4dcda8e44b5
seanbao <liangtubao@outlook.com>
2021-12-01 15:19:30 +0800

    add util 'gitlab-batch-clone'

---
+++ gitlab/batch-clone.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+import json
+import subprocess, shlex
+
+url = urlopen("http://10.1.1.243/api/v4/groups/pet-medical/projects?private_token=PRIVATE_TOKEN&per_page=1000")
+allProjects = json.loads(url.read().decode())
+for project in allProjects: 
+    try:
+        projectUrl  = project['ssh_url_to_repo']
+        command     = shlex.split('git clone --recurse-submodules %s' % projectUrl)
+        retCode  = subprocess.Popen(command)
+
+    except Exception as e:
+        print("Error on %s: %s" % (projectUrl, e.strerror))
